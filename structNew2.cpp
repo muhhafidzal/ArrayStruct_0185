527a870e7cb21ac489d36395cfbce710fea6d914
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-04-06 16:40:55 +0700

    menambahkan input dan tampilan data struct orang

--- structNew2.cpp
+++ structNew2.cpp
@@ -14,3 +14,23 @@ struct orang
     detailAlamat alamat;
     int umur;
 };
+
+int main() {
+    orang mhs;
+    cout << "Nama : " ;
+    cin >> mhs.nama;
+    cout << "alamat desa : " ;
+    cin >> mhs.alamat.desa;
+    cout << "alamat kota : " ;
+    cin >> mhs.alamat.kota;
+    cout << "umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+
+    // Tampilan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Nama : " << mhs.alamat.desa << endl;
+    cout << " Nama : " << mhs.alamat.kota << endl;
+    cout << " Nama : " << mhs.umur << endl;
+
+}
