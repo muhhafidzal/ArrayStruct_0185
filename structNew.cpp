475ca83074e32b90717c5a761f3a5086ec0e6b08
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-04-06 16:24:36 +0700

    menambahkan tampilan data struct orang

--- structNew.cpp
+++ structNew.cpp
@@ -18,5 +18,9 @@ int main() {
     cout << "umur : " ;
     cin >> mhs.umur;
     cout << endl;
-    
+
+    // Tampilan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Nama : " << mhs.alamat << endl;
+    cout << " Nama : " << mhs.umur << endl;
 }
